2081aec2474fe0c324af2bc64d00e2455e2e2ed9
Ertuğrul <33760141+curlyeto@users.noreply.github.com>
2023-03-18 00:01:41 -0400

    Added more comment line getView method

--- app/src/main/java/com/example/todolistapp/adapter/TodoAdapter.kt
+++ app/src/main/java/com/example/todolistapp/adapter/TodoAdapter.kt
@@ -44,18 +44,26 @@ class TodoAdapter(var activity: Activity, private var items:ArrayList<Todo>):Bas
         // We connect the custom layout we created here to the view holder. Because we defined views in view holder
         val view:View?
         val viewHolder:ViewHolder
+      
         if (convertView==null){
+              // If there is no view at first, we connect our layout to the viewholder via layoutinflater.
             val inflater= activity.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
             view=inflater.inflate(R.layout.list_item,null)
             viewHolder=ViewHolder(view)
             view.tag=viewHolder
         }else{
+            // If there is a view, this time we connect the existing view in view holder.
             view=convertView
             viewHolder=view.tag as ViewHolder
         }
+        // We make all the videos in the layout that we have custom made their functionality here.
+        // We assign the title variable in our todo model list array to the textview in our layout
         viewHolder.title?.text=items[position].title
+        
         viewHolder.deleteButton?.setOnClickListener {
+            // when the user presses the delete icon. We delete the item he clicked first from the list.
             items.removeAt(position)
+            // Then we report this to the view because the data has changed.
             notifyDataSetChanged()
         }
         return  view as View
